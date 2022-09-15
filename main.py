4cadeef6c2e4334d5e827a57c22952df6130ba10
aaxlss <axl9305@gmail.com>
2022-09-15 14:13:52 +0100

    [Update] Adding models for User entity

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
+#python
+from datetime import date
+from uuid import UUID
+from typing import Optional
+
+#Pydantic
+
+from pydantic import BaseModel
+from pydantic import EmailStr, Field
+#FastAPI
 from fastapi import FastAPI
 from fastapi import status
+
 app = FastAPI()
+
+#Models
+class User(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+    
+    
+class UserIn(User):
+    password: str = Field(..., min_length=8)
+
+class UserOut(User):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+        )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+        )
+    birth_day: Optional[date] = Field(default=None)
+
+class Tweet(BaseModel):
+    pass
 
 @app.get(
     path='/',
